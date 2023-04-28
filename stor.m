6490013a376d9fdc36614b9444ec4e99ad3325fe
WimpyBaby <83958291+WimpyBaby@users.noreply.github.com>
2023-04-28 15:09:15 +0200

    Add files via upload
    felfunktion used for experimental disturbance

---
+++ stor.m
@@ -0,0 +1,13 @@
+L = felfunk([60, 75, 42], [45, 88, 57]);
+
+%% 
+L1 = felfunk([65, 75, 42], [45, 88, 57]);
+L2 = felfunk([60, 80, 42], [45, 88, 57]);
+L3 = felfunk([60, 75, 47], [45, 88, 57]);
+L4 = felfunk([60, 75, 42], [50, 88, 57]);
+L5 = felfunk([60, 75, 42], [45, 93, 57]);
+L6 = felfunk([60, 75, 42], [45, 88, 62]);
+
+Stor = abs(L1-L)+abs(L2-L)+abs(L3-L)+abs(L4-L)+abs(L5-L)+abs(L6-L);
+
+disp("störningen blir " + Stor)
